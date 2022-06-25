0af8816302d765fb0d9617d39a68e976424336cc
arise-project <pirogov.e@gmail.com>
2022-06-25 02:28:08 +0000

    srvices di

--- Startup.cs
+++ Startup.cs
@@ -9,13 +9,26 @@ using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
-using aspose_health_check.Git;
-using aspose_health_check.Helper;
-using aspose_health_check.Models.Settings;
-using aspose_health_check.Parser.Interface;
-using aspose_health_check.Parser;
-using aspose_health_check.Service.Interface;
-using aspose_health_check.Translate;
+using notes_sync.Service;
+using notes_sync.Service.Interface;
+using notes_sync.Backup;
+using notes_sync.Backup.Interface;
+using notes_sync.Clean;
+using notes_sync.Clean.Interface;
+using notes_sync.Init;
+using notes_sync.Init.Interface;
+using notes_sync.Package;
+using notes_sync.Package.Interface;
+using notes_sync.Repo;
+using notes_sync.Repo.Interface;
+using notes_sync.Ressilience;
+using notes_sync.Ressilience.Interface;
+using notes_sync.Script;
+using notes_sync.Script.Interface;
+using notes_sync.Sd;
+using notes_sync.Sd.Interface;
+using notes_sync.Structure;
+using notes_sync.Structure.Interface;
 
 namespace notes_sync
 {
@@ -32,9 +45,51 @@ namespace notes_sync
         {
             services.AddOptions();
             //services.Configure<AppConfig>(Configuration.GetSection("AppConfig"));
+	    
+	    //Services DI
+	    services.AddSingleton<InitBuilder, nitBuilder>();
+		services.AddSingleton<ImportantScanner, mportantScanner>();
 
-            //services.AddSingleton((f) => BuildSettings());
-            //services.AddSingleton<IResourceWritter, TomlWritter>();
+		services.AddSingleton<IArchiveManager, ArchiveManager>();
+		services.AddSingleton<INoteBackup, NoteBackup>();
+		services.AddSingleton<ITextBackup, TextBackup>();
+
+		services.AddSingleton<ICompressManager, CompressManager>();
+		services.AddSingleton<IContentCleaner, ContentCleaner>();
+		services.AddSingleton<IEraser, Eraser>();
+		services.AddSingleton<IFileCleaner, FileCleaner>();
+		services.AddSingleton<IFolderCleaner, FolderCleaner>();
+
+		services.AddSingleton<IInitBuilder, InitBuilder>();
+		services.AddSingleton<ISingleInitBuilder, SingleInitBuilder>();
+
+		services.AddSingleton<IDebDependencyManager, DebDependencyManager>();
+		services.AddSingleton<IDebManager, DebManager>();
+
+		services.AddSingleton<IDiffManager, DiffManager>();
+		services.AddSingleton<IStructureRepositoryManager, StructureRepositoryManager>();
+
+		services.AddSingleton<IHardener, Hardener>();
+		services.AddSingleton<IHardSearcher, HardSearcher>();
+		services.AddSingleton<IHardStructureManager, HardStructureManager>();
+		services.AddSingleton<IImportantScanner, ImportantScanner>();
+
+		services.AddSingleton<IProcessRunner, ProcessRunner>();
+		services.AddSingleton<IScriptManager, ScriptManager>();
+
+		services.AddSingleton<ICapacityManager, CapacityManager>();
+		services.AddSingleton<IFolderMerger, FolderMerger>();
+		services.AddSingleton<IFolderWalker, FolderWalker>();
+
+		services.AddSingleton<ICorrector, Corrector>();
+		services.AddSingleton<IDefaultStructureScanner, DefaultStructureScanner>();
+		services.AddSingleton<INoteParagraphManager, NoteParagraphManager>();
+		services.AddSingleton<INoteStructureChecker, NoteStructureChecker>();
+		services.AddSingleton<IReportManager, ReportManager>();
+		services.AddSingleton<IStructureMerger, StructureMerger>();
+		services.AddSingleton<IStructureSearcher, StructureSearcher>();
+            
+	    //services.AddSingleton((f) => BuildSettings());
         }
 
         public void Configure(
