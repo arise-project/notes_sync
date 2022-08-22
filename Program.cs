a3e7c3799ca2586bfd71810ec1b11035865e9449
Evgeny.Pirogov@aspose.com <2WHh8RR%V2:2\MU>
2022-08-22 23:48:51 +0300

    build

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace notes_sync
             if (File.Exists("./log.txt"))
                 File.Delete("./log.txt");
 
-            CreateHostBuilder(args).Build().Run(args);
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- Service/Sd/FolderWalker.cs
+++ Service/Sd/FolderWalker.cs
@@ -1,12 +1,11 @@
 using notes_sync.Services.Sd.Interface;
 using notes_sync.Model;
-
 using Microsoft.Extensions.Options;
 using notes_sync.Config;
 
 namespace notes_sync.Services.Sd
 {
-    public class FolderWalker : IFolderMerger
+    public class FolderWalker : IFolderWalker
     {
         readonly AppConfig conf;
         public FolderWalker(IOptions<AppConfig> conf)
